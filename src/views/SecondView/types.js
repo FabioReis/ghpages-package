97ef2fb6ce86c67ccc14c3047534458f9dd489e0
FabioReis <fabiojrodrigues.r@gmail.com>
2022-06-10 23:08:20 +0900

    test deature releases

---
+++ src/views/SecondView/types.js
@@ -0,0 +1,9 @@
+/**
+ * Created by liuqi453 on 10/20/16.
+ */
+// define names of getters, actions and mutations as constants
+// and they are prefixed by the module name `secondView`
+
+export const DEFAULT_STATE = 'secondView/DEFAULT_STATE'
+
+export const GETTER_STATE = 'secondView/DEFAULT_STATE'
